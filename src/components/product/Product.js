66f595316c1d0d2bd861ac64559f7312b260d907
ramseenakomath <ramseena1832@gmail.com>
2023-09-13 20:30:11 +0530

    initial commit

---
+++ src/components/product/Product.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { productData } from './ProductData';
+import "./Product.css"
+
+const Product = () => {
+
+  const [items,setItems] =useState(productData);
+  const [cart,setCart] = useState([]);
+  const [totalPrice,setTotalPrice] = useState(0);
+
+  const addTOCart =(item) => {
+    const sameInCArt = cart.some((product) => product.id === item.id);
+    if(!sameInCArt){
+      const newCart = [...cart,item];
+      setCart(newCart);
+      setTotalPrice(totalPrice + item.price);
+    }
+  }
+
+  const removeFromCart = (item) => {
+    const newCart = cart.filter((product) => product.id !== item.id);
+    setCart(newCart);
+    setTotalPrice(totalPrice - item.price);
+  }
+
+  return (
+    <>
+      <div>
+        
+        <div className='products' id='home'>
+          <div className='header'>
+            <h1 className='heading'>Product Hub</h1>
+            <button className='sec-btn'><a href='#cart'>Cart</a></button>
+          </div>
+            {items.map((item) => (
+              <div className='items' key={item.id}>
+                <img alt='' className='image' src={require(`./laps/${item.image}.png`)}/>
+                <div className='item-body'>
+                  <h2>{item.name}</h2>
+                  <h4>{item.description}</h4>
+                  <h3>Price: ₹ {item.price} </h3>
+                  <h4>Stock: {item.stock}</h4>
+                  <button className='addcart-btn' onClick={() => addTOCart(item)}>Add to Cart</button>
+                </div>              
+              </div>
+            ))}
+        </div>
+
+        <div className='cart-page hide' id='cart'>
+          <div className='header'>
+            <h1>Shopping Cart</h1>
+            <button className='sec-btn'><a href='#home'>Home</a></button>
+          </div>
+          <div className='body'>
+            <h2 className='total'>Total Price: {totalPrice}</h2>
+            {cart.map((item) => (
+              <div className='cart' key={item.id}>
+                <img alt='' className='image' src={require(`./laps/${item.image}.png`)}/>
+                <div className='cart-body'>
+                  <h2>{item.name}</h2>
+                  <h4>{item.description}</h4>
+                  <h3>Price: ₹ {item.price} </h3>
+                  <button className='remove-btn' onClick={()=> removeFromCart(item)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Product
---
+++ src/components/product/ProductData.js
@@ -0,0 +1,51 @@
+export const productData = [
+  {
+    id:1,
+    name:"Lenovo",
+    description:"Lenovo Yoga 6 Gen 7 (13, AMD) 16GB RAM/512 GB SSD",
+    price: 82900,
+    image:"Lenovo",
+    stock:10
+  },
+  {
+    id:2,
+    name:"HP",
+    description:"HP 15s, 12th Gen Intel Core i5 16GB RAM/512GB SSD",
+    price: 62000,
+    image:"HP",
+    stock:15
+  },
+  {
+    id:3,
+    name:"ASUS",
+    description:"Asus Vivobook S15 | i5 12th Gen/16GB RAM/512GB SSD",
+    price: 75000,
+    image:"ASUS",
+    stock:10
+  },
+  {
+    id:4,
+    name:"Dell",
+    description:"Dell Inspiron 5301 Intel i5-1135G7/8 GB / 1TB SSD/",
+    price: 60000,
+    image:"Dell",
+    stock:20
+  },
+  {
+    id:5,
+    name:"Acer",
+    description:"Acer Swift Go 14 OLED 16GB/ 512 GB SSD",
+    price: 72900,
+    image:"Acer",
+    stock:8
+  },
+  {
+    id:6,
+    name:"Apple",
+    description:"APPLE 2022 MacBook Pro M2 | 8 GB/256 GB SSD",
+    price: 122900,
+    image:"Apple",
+    stock:12
+  },
+  
+]
